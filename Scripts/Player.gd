3082435f48a1476a84db7fa7127857c2395b2036
Harshharsh2275 <hpathak874@gmail.com>
2021-01-12 16:46:54 +0530

    Added zoom on player when running

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -1,12 +1,14 @@
 extends KinematicBody2D
 
-const yDIRECTION = Vector2(0,-1)
+const yDIRECTION = Vector2(0,-1) #can use vector2.UP
 const ACCELARATION = 375
 const MAX_SPEED = 750
 const GRAVITY = 35
 const JUMP_SPEED = 1300
 var motion = Vector2()
 var dead = false
+
+onready var player_cam: Camera2D = $Camera2D #camera for the main character
 
 func _physics_process(delta):
 	if dead == false:
@@ -15,13 +17,17 @@ func _physics_process(delta):
 		motion.y += GRAVITY
 		if Input.is_action_pressed("ui_right"):
 			motion.x = min(motion.x+ACCELARATION,MAX_SPEED)
-			$Sprite.flip_h = false # no flip 
+			$Sprite.flip_h = false # no flip
+			_change_zoom(Vector2(2,2))
 			$Sprite.play("run") 
+			
 		elif Input.is_action_pressed("ui_left"):
 			motion.x = max(motion.x-ACCELARATION,-MAX_SPEED)
 			$Sprite.flip_h = true #flip when left pressed
+			_change_zoom(Vector2(2,2))
 			$Sprite.play("run") 
 		else:
+			_change_zoom(Vector2(1.8,1.8))
 			$Sprite.play("idle")
 			friction = true
 		if is_on_floor():
@@ -40,7 +46,7 @@ func _physics_process(delta):
 				motion.x = lerp(motion.x, 0 ,0.05) #smooth stop
 		if self.position.y > 120:
 			drown()
-		motion = move_and_slide(motion,yDIRECTION)
+		motion = move_and_slide(motion,Vector2.UP)
 		if global.player_health <= 0:
 			die()
 
@@ -64,3 +70,8 @@ func _on_Timer_timeout():
 		global.player_health = 100
 		self.position = global.check_point
 		dead = false
+		
+func _change_zoom(zoom: Vector2) -> void:
+	#change the zoom for the camera
+	player_cam.zoom.x = lerp(player_cam.zoom.x, zoom.x, 0.05)
+	player_cam.zoom.y = lerp(player_cam.zoom.y, zoom.y, 0.05)
